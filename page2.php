b9a1094386d44653f8fa05198cbfefcb2970ef63
diksha12 <diksha32@live.com>
2014-10-01 23:34:36 +0530

    add

--- page2.php
+++ page2.php
@@ -11,12 +11,13 @@
 		height: 730px;
 		background-color: black;	
 	}
+	
 	.logo
 	{
 		height: 118px;
 		width: 286px;
 		position: relative;
-		margin-top: -760px;
+		background-color: blue;
 		margin-left: 232px;
 		background-image: url(./logo.jpg);
 		background-repeat: no-repeat;
@@ -25,14 +26,35 @@
 		background-size: 285px;
 		cursor: pointer;
 	}
+	.credits
+	{
+		width: 300px;
+		height: 500px;
+		display: inline-block;
+		margin-left: 1101px;
+		margin-top: -272px;
+		background-color: rgb(224, 14, 14);
+		border-radius: 28px;
+	}
+	.List
+	{
+		color: rgb(237, 223, 0);
+		font-size: 32px;
+		margin-top: 187px;
+		cursor: pointer;
+	}
 	</style>
 </head>
 
 <body>
 
 <div class="box">
-	<div class="logo">
-	</div>
+	<div class="logo"></div>
+	<ul class="List">
+		<li><H1><u><i>Play Your Quiz</i></u></H1>
+		<li><H1><u><i>Create Your Qwn Quiz</i></u></H1>
+	</ul>
+	<div class="credits"></div>
 </div>
 
 </body>
